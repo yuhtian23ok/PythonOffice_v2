a702bd738f6505ee008bcc7c4b08e3c4fb3ed1b7
Jason <yuhtian23ok@gmail.com>
2024-03-23 16:23:48 +0800

    update

--- MailSend.py
+++ MailSend.py
@@ -4,20 +4,21 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 sender = "yuhtian23ok@gmail.com"
-receiver = "ctho64571@gmail.com"
+receiver_list = ["ctho64571@gmail.com"]
 
-msg = MIMEMultipart()
-msg['From'] = sender
-msg['To'] = receiver
-msg['Subject'] = Header("Test of sending email", "utf-8").encode()
+for receiver in receiver_list:
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg['Subject'] = Header("Test of sending email", "utf-8").encode()
 
-body = "This is a test email sended by Python"
+    body = "This is a test email sended by Python"
 
-msg.attach(MIMEText(body))
+    msg.attach(MIMEText(body))
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as server:
-    server.login(sender,"qqtrniorianwdjka")
-    server.sendmail(sender, receiver, msg.as_string())
-print("Send email success!")
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as server:
+        server.login(sender,"qqtrniorianwdjka")
+        server.sendmail(sender, receiver, msg.as_string())
+    print("Send email success!")
 
